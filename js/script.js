0497f9b85ae445016c3b927b3c814111c83c7a60
trojanspot <taufik.ph0814@gmail.com>
2022-01-28 21:29:21 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 5, 2022 00:00:00").getTime();
+var countDownDate = new Date("Feb 05, 2022 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
