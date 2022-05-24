6ade5ec705d2ddcda8b23f7d8989090bf87c6fad
trojanspot <taufik.ph0814@gmail.com>
2022-05-24 15:02:38 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("May 16, 2022 00:00:00").getTime();
+var countDownDate = new Date("May 26, 2022 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -85,7 +85,7 @@ var styles2 = [
     , 'font-size: 32px'
 ].join(';');
 
-console.log('\n\n%c INGAT TANGGALNYA: 16 Mei 2022!', styles);
+console.log('\n\n%c INGAT TANGGALNYA: 26 Mei 2022!', styles);
 
 console.log('%cYour presence is requested!%c\n\nRegards: AsahanTECH', styles1, styles2);
 
